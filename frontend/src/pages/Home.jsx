5f6a80e8cc9bd551a3c2cf30a790fda22ffb6a80
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-07-22 16:13:51 +0100

    Load the movies list from the api

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,29 @@
 import MovieCard from "../components/MovieCard";
-import  { useState } from "react";
+import  { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
 
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        { id: 1, title: "Aquaman", release_date: "2022" },
-        { id: 2, title: "Titanic", release_date: "1997" },
-        { id: 3, title: "Tenet", release_date: "2023" },
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadPopularMovies()
+    }, []);
 
     const handleSearch = (e) => {
         e.preventDefault();
