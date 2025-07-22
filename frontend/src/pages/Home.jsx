49043f083a1c20307b352147c50cf8b5f3ea20f1
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-07-22 14:58:08 +0100

    Add search form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import MovieCard from "../components/MovieCard";
+import  { useState } from "react";
 
 function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "Aquaman", release_date: "2022" },
         { id: 2, title: "Titanic", release_date: "1997" },
         { id: 3, title: "Tenet", release_date: "2023" },
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
     }
 
     return (
@@ -17,12 +22,14 @@ function Home() {
                 <input type="text"
                     placeholder="Search for movies ..."
                     className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className="movies-grid">
                 {movies.map(movie => (
-                    <MovieCard movie={movie} key={movie.id} />
+                    movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
         </div>
