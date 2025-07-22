20df1d9e0992389e7ab5e0dc7c2d3460704912cd
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-07-22 15:01:30 +0100

    Create favorite movies component

---
+++ frontend/src/pages/FavoriteMovies.jsx
@@ -0,0 +1,10 @@
+function FavorteMovies() {
+    return (
+        <div className="favorites-empty">
+            <h2>No favorite movies yet</h2>
+        </div>
+        
+    );
+}
+
+export default FavorteMovies;
