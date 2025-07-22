67bf8523c97888d759b01650be59a240add9ad76
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-07-22 15:14:26 +0100

    Add navbar component:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
 import FavoriteMovies from './pages/FavoriteMovies';
 import { Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
 
 function App() {
-  const movieNumber = 1;
-
   return (
     <>
+    <NavBar />
       <main className='main-content'>
         <Routes>
           <Route path='/' element={<Home/>}/>
