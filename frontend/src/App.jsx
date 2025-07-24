b33520a9011e315a8ec9be4d974d28c144505b03
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-07-24 20:45:58 +0100

    Add MovieProvider to app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import './css/App.css';
 import Home from './pages/Home';
 import FavoriteMovies from './pages/FavoriteMovies';
 import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from './contexts/MovieContext';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <>
+    <MovieProvider>
     <NavBar />
       <main className='main-content'>
         <Routes>
@@ -14,7 +15,7 @@ function App() {
           <Route path='/favorite-movies' element={<FavoriteMovies />}/>
         </Routes>
       </main>
-    </>
+    </MovieProvider>
   )
 }
 
